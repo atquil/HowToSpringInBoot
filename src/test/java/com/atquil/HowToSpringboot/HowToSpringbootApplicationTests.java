9034363d589fad7d3588a1163f726f8c1609c785
aulanand <atulanandmishra@gamil.com>
2022-12-17 20:08:13 +0530

    How to Spring Boot

---
+++ src/test/java/com/atquil/HowToSpringboot/HowToSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atquil.HowToSpringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
